7182e61999e37fd622e9534b79f2599efba6941b
Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>
2023-05-03 15:58:32 -0400

    update

---
+++ src/main/java/com/bufigol/constantes/DBConstantes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.bufigol.constantes;
+
+/**
+ *
+ * @author Bufigol
+ */
+public interface DBConstantes {
+
+    public static final String DB_URL = "jdbc:apache:commons:dbcp:mydatasource";
+    public static final String DB_USER = "myusername";
+    public static final String DB_PASSWORD = "mypassword";
+    public static final String APACHECLASSNAME = "org.apache.commons.dbcp2.PoolingDataSource";
+}
---
+++ src/main/java/com/bufigol/constantes/MessageConstants.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.bufigol.constantes;
+
+/**
+ *
+ * @author Bufigol
+ */
+public interface MessageConstants {
+
+    public static final String SQLExceptionErrorMsg = "Could not connect to database.";
+    public static final String DBConected ="Connected to database successfully!";
+}
